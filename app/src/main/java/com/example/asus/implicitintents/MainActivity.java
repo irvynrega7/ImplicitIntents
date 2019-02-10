32f1a6b0ce7cd40fbf1890d67b78b106a3f6dbbc
Irvyn Rega <irvynrega7@gmail.com>
2019-02-10 23:06:20 +0700

    Implement the the Share This Text Button

--- app/src/main/java/com/example/asus/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/asus/implicitintents/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.asus.implicitintents;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +12,15 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebsiteEditText = findViewById(R.id.website_edittext);
         mLocationEditText = findViewById(R.id.location_edittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
     }
 
     public void openWebsite(View view) {
@@ -53,5 +57,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder.from(this).setType(mimeType).setChooserTitle("Share this text with: ").setText(txt).startChooser();
     }
 }
